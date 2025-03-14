d2b84e720d584be74cd573f74c46fe0f02f0190d
Dr. Farshid PirahanSiah <pirahansiah@gmail.com>
2025-03-14 20:21:50 +0100

    update

---
+++ farshid/content/pyrun2.py
@@ -0,0 +1,7 @@
+#!/Users/farshid/projects/install/miniconda3/envs/farshid/bin/python
+import sys
+
+if __name__ == "__main__":
+    for arg in reversed(sys.argv[1:]):
+        print(arg)
+        #!/usr/bin/env /Users/farshid/projects/install/miniconda3/envs/farshid/bin/python
